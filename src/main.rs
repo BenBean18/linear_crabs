f8ae2141a9735e12754ae7f4c5a7d5a431e69d95
BenBean18 <beakerthespeaker@gmail.com>
2023-08-31 16:49:47 -0400

    Add a function to print matrices and add comments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-mod matmul;
+mod math;
 
 fn main() {
-    println!("Hello, world!");
-    let mat = matmul::multiply(vec![vec![1., 0.], vec![-2., 1.]], vec![vec![1., 3.], vec![2., 4.]]);
-    for row in mat {
-        for e in row {
-            print!("{e} ");
-        }
-        println!();
-    }
+    let mat = math::matmul(vec![vec![1., 0.], vec![-2., 1.]], vec![vec![1., 3.], vec![2., 4.]]);
+    math::print_mat(mat);
+    println!();
+    math::print_mat(math::matmul(vec![vec![1., 2., 4.]], vec![vec![2.], vec![2.], vec![2.]]));
 }
---
+++ src/math.rs
@@ -0,0 +1,54 @@
+use std::vec;
+
+pub(crate) fn matmul(a: Vec<Vec<f64>>, b: Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    // Find the dimensions of a and b (rows x columns)
+    let a_dims = [a.len(), a[0].len()];
+    let b_dims = [b.len(), b[0].len()];
+
+    // If a's columns do not match b's rows, we cannot do this matrix multiplication
+    if a_dims[1] != b_dims[0] {
+        println!("Inner dimensions DO NOT match, we cannot multiply these matrices");
+        return vec![]
+    }
+
+    // Create a variable for the output matrix
+    let mut output: Vec<Vec<f64>> = vec![];
+
+
+    // The "row operations" way of thinking of matrix multiplication:
+    // - Each row in A represents what that row will be in the new matrix, in terms of the rows of B.
+    // - e.g. A = 1 0 = I
+    // -          0 1
+    // -      B = 1 2
+    // -          3 4
+    // - The first row of A, [1 0], says that the first row of the new matrix is:
+    // - 1 * first row of B + 0 * second row of B = 1 * [1 2] + 0 * [3 4]
+
+    // For each row in A, e.g. [1 0]
+    for a_row in a {
+        // Create a vector to store the current row in the output matrix
+        let mut this_row: Vec<f64> = vec![0.; b[0].len()];
+        // For each index in the row (represents scalar to multiply by corresponding row of B), index 0 = 1 in this case
+        for i in 0..a_row.len() {
+            // For each element in B's (i)th row (which is B[0] = [1 2])
+            for j in 0..b[i].len() {
+                // Multiply it (in this example, the number 1) by the (i)th element in the current row of A
+                // Add it to the current total in the (j)th spot of the row
+                this_row[j] += &b[i][j] * a_row[i];
+            }
+        }
+        // Add the finished row to the matrix
+        output.push(this_row);
+    }
+
+    output // Return the matrix (isn't rust nice)
+}
+
+pub(crate) fn print_mat(mat: Vec<Vec<f64>>) {
+    for row in mat {
+        for e in row {
+            print!("{e} ");
+        }
+        println!();
+    }
+}
