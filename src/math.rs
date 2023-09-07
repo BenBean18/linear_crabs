1add83ce22acd9dc92dcb1da69bdf9f52080dd13
BenBean18 <beakerthespeaker@gmail.com>
2023-09-06 21:54:15 -0400

    Add comments

--- src/math.rs
+++ src/math.rs
@@ -47,7 +47,7 @@ pub(crate) fn matmul(a: Vec<Vec<f64>>, b: Vec<Vec<f64>>) -> Vec<Vec<f64>> { // O
 pub(crate) fn print_mat(mat: Vec<Vec<f64>>) {
     for row in mat {
         for e in row {
-            print!("{e} ");
+            print!("{:.2} ", e);
         }
         println!();
     }
@@ -92,23 +92,23 @@ pub(crate) fn gauss_jordan(mut mat: Vec<Vec<f64>>) -> Result<Vec<Vec<f64>>, &'st
 
     while j < mat.len() as i64 { // O(n)
         // for each column, j
+
+        // set the next column to be the current one (this might be changed later)
         let mut new_j = j;
+
+        // 0...maximum down row (rows - j)...-1...maximum up row (-j)
         let mut range: Vec<i64> = (0..mat.len() as i64 - j).collect::<Vec<_>>();
         range.extend((-j..0).collect::<Vec<_>>());
 
         let mut found_working_pivot = false;
 
+        // try to find a nonzero pivot, starting with the current row and going down.
+        // we only want to go up if we REALLY need to (since that requires reelimination of the above row)
         for current_row in range { // O(n)
             if mat[j as usize][j as usize] == 0. {
-                // row exchange
+                // If the element along the main diagonal is zero, do a row exchange with current_row
+
                 // Need to handle re-elimination when you have to go above to find a pivot
-                // let mut this_elim_mat = identity(mat.len()); // O(n^2)
-                // this_elim_mat[j as usize] = vec![0.; mat.len()];
-                // this_elim_mat[j as usize][(j+current_row) as usize] = 1.;
-                // this_elim_mat[(j+current_row) as usize] = vec![0.; mat.len()];
-                // this_elim_mat[(j+current_row) as usize][j as usize] = 1.;
-                // elim_mat = matmul(this_elim_mat.clone(), elim_mat); // O(n^3)
-                // mat = matmul(this_elim_mat.clone(), mat); // O(n^3)
 
                 // This is O(n) because you are switching two rows
                 let old_next_row = &mat.clone()[(j + current_row) as usize];
@@ -117,40 +117,56 @@ pub(crate) fn gauss_jordan(mut mat: Vec<Vec<f64>>) -> Result<Vec<Vec<f64>>, &'st
 
                 // println!("Row exchange, new matrix:");
                 // print_mat(mat.clone());
+
+                // If we have to go backwards, subtract from the column variable (have to reeliminate above rows now that they are switched)
                 if current_row < 0 {
                     new_j = j + current_row;
                     // println!("Backward row exchange :(");
                 }
             } else {
+                // If there is a nonzero element in our position of the main diagonal,
+                // we have found a working pivot and should exit the loop.
                 found_working_pivot = true;
                 break;
             }
         }
+
+        // If we haven't found a working pivot, the matrix must be singular (not exactly sure how to prove this at 9:45pm)
         if !found_working_pivot {
             println!("Matrix is singular");
             return Err("Matrix is singular")
         }
+
+        // Set the pivot to the one that we found
         pivot = mat[j as usize][j as usize];
         // println!("Picked pivot {j}, {j} = {pivot}");
+
+        // If we had to move our column back, go back to the start of the loop and reeliminate all necessary above rows
         if j != new_j {
             j = new_j;
             continue;
         }
+
+        // For each row
         for i in 0..mat.len() { // O(n)
             // for each row, i
             if i != j as usize {
-                // let mut this_elim_mat = identity(mat.len()); // O(n^2)
-                // this_elim_mat[i][j as usize] = -mat[i][j as usize]/pivot;
-                // this_elim_mat[i][i] = 1.;
-                // println!("r{} = {}r{} + r{}", i+1, -mat[i][j as usize]/pivot, j+1, i+1);
-                // elim_mat = matmul(this_elim_mat.clone(), elim_mat); // O(n^3)
-                // mat = matmul(this_elim_mat.clone(), mat); // O(n^3)
 
+                // Store what we have to multiply the row containing the pivot by to zero out our row/column
+                // We'll be zeroing our row/column so if we calculate this each column it will only work the first time,
+                // then be zero.
                 let multiple = -mat[i][j as usize]/pivot;
-                // Should be O(n) since you have to do something for each element
-                for col in 0..mat[0].len() { // O(n)
+
+                // O(n) since you have to do something for each element
+                for col in 0..mat[0].len() {
+
+                    // For each column,
+
                     // println!("col {col} before = {} -> += {} * {}", mat[i][col], mat[j as usize][col], multiple);
+
+                    // Multiply the corresponding column in the pivot row by the multiplier from before, and add it to the current element
                     mat[i][col] += mat[j as usize][col] * multiple;
+
                     // println!("col {col} after = {}", mat[i][col]);
                 }
 
@@ -158,28 +174,46 @@ pub(crate) fn gauss_jordan(mut mat: Vec<Vec<f64>>) -> Result<Vec<Vec<f64>>, &'st
             }
         }
 
-        // Make identity matrix
+        // Make the eliminated matrix an identity matrix
+        // If we have a matrix representing the system:
+        // x + y + z = 6
+        // 3x + 2y + z = 10
+        // 2x + y + 2z = 10
+        
+        // aka
+        // 1 1 1 6
+        // 3 2 1 10
+        // 2 1 2 10
+
+        // then making the square part an identity matrix yields
+        // 1 0 0 a
+        // 0 1 0 b
+        // 0 0 1 c
+
+        // or:
+        // x = a
+        // y = b
+        // z = c
         for i in 0..mat.len() { // O(n)
             // for each row, i
             if i == j as usize {
-                // let mut this_elim_mat = identity(mat.len()); // O(n^2)!!
-                // this_elim_mat[i][i] = 1./mat[i][i];
-                // I didn't comment out identity matrix creation before so it was stuck at O(n^4)
+                let multiple = 1./mat[i][i]; // Store what we have to multiply by to make the row a row of the identity matrix
 
-                let multiple = 1./mat[i][i];
-                // Should be O(n) since you have to do something for each element
+                // O(n) since you have to do something for each element
                 for col in 0..mat[0].len() { // O(n)
+                    // Multiply each element in the row by the above multiplier
                     mat[i][col] *= multiple;
                 }
-                // elim_mat = matmul(this_elim_mat.clone(), elim_mat); // O(n^3)
-                // mat = matmul(this_elim_mat.clone(), mat); // O(n^3)
             }
         }
 
         // print_mat(mat.clone());
+
+        // Increment the column
         j += 1;
     }
 
+    // Return the eliminated matrix
     return Ok(mat)
 }
 
@@ -190,6 +224,7 @@ pub(crate) fn random(n: usize) -> Vec<Vec<f64>> {
         for j in 0..n {
             mat[i][j] = (rng.gen::<i8>() as f64);
         }
+        mat[i].push((rng.gen::<i8>() as f64));
     }
     mat
 }
